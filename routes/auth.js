32cfdc9b1c46a0e8a6320d5363f317a749c37320
Iceberg1109 <smartdev951109@gmail.com>
2020-01-09 01:27:35 +0800

    init auth

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const AuthController = require('../controller/auth');
+//When the user sends a post request to this route, passport authenticates the user based on the
+//middleware created previously
+router.post('/signup', passport.authenticate('signup', { session : false }) , async (req, res, next) => {
+  res.json({ 
+    message : 'Signup successful',
+    user : req.user 
+  });
+});
+
+router.post('/login', async (req, res, next) => {
+  await AuthController.login(req, res, next);
+});
+
+module.exports = router;
