dca4363e91c546ba5c0967c7d3ed3dbf2e878b28
iceberg1109 <smartdev951109@gmail.com>
2020-01-26 21:32:38 +0800

    code review

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -1,19 +1,18 @@
 const UserModel = require('../model/user.model');
 
 module.exports = {
-    getUsersList: function (req, res) {
-      UserModel.find({isAdmin: false}, 'name email', function(err, users) {
-        if (err) res.json({status: "fail"});  
-        var userMap = {};
-        
-        var idx = 0;
-        users.forEach(function(user) {
-          userMap[idx] = {user};
-          idx ++;
-        });
-    
-        res.json({status: "success",count: idx, users: userMap});  
+  getUsersList: function (req, res) {
+    UserModel.find({isAdmin: false}, 'name email', function(err, users) {
+      if (err) res.json({status: "fail"});  
+      var userMap = {};
+      
+      var idx = 0;
+      users.forEach(function(user) {
+        userMap[idx] = {user};
+        idx ++;
       });
+  
+      res.json({status: "success",count: idx, users: userMap});  
+    });
   },
-
 }
